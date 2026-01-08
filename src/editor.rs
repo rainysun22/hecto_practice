aabe535ea189209080b7e208f74795f511baef6b
rainysun22 <54364883@qq.com>
2026-01-08 16:04:51 +0800

    init practice

---
+++ src/editor.rs
@@ -0,0 +1,47 @@
+use crossterm::event::{Event, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers, read};
+use crossterm::execute;
+use crossterm::terminal::{Clear, ClearType, disable_raw_mode, enable_raw_mode};
+use std::io::stdout;
+
+pub struct Editor {
+    should_quit: bool,
+}
+
+impl Editor {
+    pub fn default() -> Self {
+        Editor { should_quit: false }
+    }
+
+    pub fn run(&mut self) {
+        Self::initialize().unwrap();
+        let result = self.repl();
+        Self::terminate().unwrap();
+        result.unwrap();
+    }
+
+    fn initialize() -> Result<(), std::io::Error> {
+        enable_raw_mode()?;
+        Self::clear_screen()
+    }
+
+    fn terminate() -> Result<(), std::io::Error> {
+        disable_raw_mode()
+    }
+
+    fn clear_screen() -> Result<(), std::io::Error> {
+        let mut stdout = stdout();
+        execute!(stdout, Clear(ClearType::All))
+    }
+
+    fn repl(&mut self) -> Result<(), std::io::Error> {
+        loop {
+            let event = read()?;
+            self.evaluate_event(&event);
+            self.refresh_screen()?;
+            if self.should_quit {
+                break;
+            }
+        }
+        Ok(())
+    }
+}
